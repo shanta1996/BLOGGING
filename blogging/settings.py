bb7d4423fbfd84b76c1425050ca8ef34fbc23d56
shanta1996 <shantachaudhary490@gmail.com>
2024-11-28 18:27:14 +0545

    changes

--- blogging/settings.py
+++ blogging/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--*qh&#1i*y%n+2$stqu3cqi@tg+b2_lmhrc0mx@7o0h0uq@2)5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
 
 
 # Application definition
